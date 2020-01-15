6afb8d9ab21e2116cf2f95a47fbf72fe914c4a8d
Hiago Souza <hiagofss98@gmail.com>
2020-01-14 22:20:20 -0300

    Update DevController add Location and save Dev and validation Dev exists

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -3,23 +3,30 @@ const axios = require('axios');
 
 class DevController {
   async create(req, res) {
-    const { github_username, techs } = req.body;
+    const { github_username, techs, latitude, longitude } = req.body;
 
-    const apiResponse = await axios.get(
-      `https://api.github.com/users/${github_username}`
-    );
+    let dev = await DevSchema.findOne({ github_username });
 
-    const { name = login, bio, avatar_url } = apiResponse.data;
+    if (!dev) {
+      const apiResponse = await axios.get(
+        `https://api.github.com/users/${github_username}`
+      );
 
-    const techsArray = techs.split(',').map(tech => tech.trim());
+      const { name = login, bio, avatar_url } = apiResponse.data;
 
-    const dev = await DevSchema.create({
-      name,
-      bio,
-      avatar_url,
-      github_username,
-      techs: techsArray,
-    });
+      const techsArray = techs.split(',').map(tech => tech.trim());
+      dev = await DevSchema.create({
+        name,
+        bio,
+        avatar_url,
+        github_username,
+        techs: techsArray,
+        location: {
+          type: 'Point',
+          coordinates: [longitude, latitude],
+        },
+      });
+    }
 
     return res.json(dev);
   }
